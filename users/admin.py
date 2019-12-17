ea58f8e572379987491e24ef4523f6585b03f17c
saimounikanedunuri <saimounikanedunuri@gmail.com>
2019-12-17 15:02:08 +0530

    first commit

---
+++ users/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from import_export.admin import ImportExportModelAdmin
+from.models import *
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+
+
+class ClassInline(admin.TabularInline):
+    model = Class
+    extra = 0
+
+
+class DeptAdmin(admin.ModelAdmin):
+    inlines = [ClassInline]
+    list_display = ('name', 'id')
+    search_fields = ('name', 'id')
+    ordering = ['name']
+
+
+class StudentInline(admin.TabularInline):
+    model = Student
+    extra = 0
+
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dept', 'sem', 'section')
+    search_fields = ('id', 'dept__name', 'sem', 'section')
+    ordering = ['dept__name', 'sem', 'section']
+    inlines = [StudentInline]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'dept')
+    search_fields = ('id', 'name', 'dept__name')
+    ordering = ['dept', 'id']
+
+#@admin.register(Student)
+class StudentAdmin(ImportExportModelAdmin):
+    list_display = ('USN', 'name', 'class_id','prntphno', 'studphno')
+    search_fields = ('USN', 'name', 'class_id__id', 'class_id__dept__name')
+    ordering = ['class_id__dept__name', 'class_id__id', 'USN']
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dept')
+    search_fields = ('name', 'dept__name')
+    ordering = ['dept__name', 'name']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Dept, DeptAdmin)
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Teacher, TeacherAdmin)
---
+++ users/models.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from phone_field import PhoneField
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+
+sex_choice = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+
+class Dept(models.Model):
+    id = models.CharField(primary_key='True', max_length=100)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    dept = models.ForeignKey(Dept, on_delete=models.CASCADE)
+    id = models.CharField(primary_key='True', max_length=50)
+    name = models.CharField(max_length=50)
+    shortname = models.CharField(max_length=50, default='X')
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    # courses = models.ManyToManyField(Course, default=1)
+    id = models.CharField(primary_key='True', max_length=100)
+    dept = models.ForeignKey(Dept, on_delete=models.CASCADE)
+    section = models.CharField(max_length=100)
+    sem = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'classes'
+
+    def __str__(self):
+        d = Dept.objects.get(name=self.dept)
+        return '%s : %d %s' % (d.name, self.sem, self.section)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE, default=1)
+    USN = models.CharField(primary_key='True', max_length=100)
+    name = models.CharField(max_length=200)
+    sex = models.CharField(max_length=50, choices=sex_choice, default='Male')
+    DOB = models.DateField(default='1998-01-01')
+    prntphno = PhoneField(blank="True", help_text="Contact parent phone number")
+    studphno = PhoneField(blank="True", help_text="Contact student phone number")
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    id = models.CharField(primary_key=True, max_length=100)
+    dept = models.ForeignKey(Dept, on_delete=models.CASCADE, default=1)
+    name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=50, choices=sex_choice, default='Male')
+    DOB = models.DateField(default='1998-01-01')
+
+    def __str__(self):
+        return self.name
+
